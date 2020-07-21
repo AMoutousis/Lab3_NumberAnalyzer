35cc8a74a56b0df3d42c58c85122c0188f7552da
Swoopsy <38360122+Swoopsy@users.noreply.github.com>
2020-07-21 17:18:25 -0400

    Initial Commit

---
+++ Lab3_NumberAnalyzer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lab3_NumberAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numAnalyzer = 2;
+            bool validNum = false;
+            bool again = true;
+
+            Console.WriteLine("Hello! So we can get to know each other, what is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userName);
+
+
+            while (again)
+            {
+                Console.WriteLine("Please enter a whole number between 1 and 100");
+                double temp = double.Parse(Console.ReadLine());
+
+                while (userNum < 0 || userNum > 100)
+                {
+                    Console.WriteLine("Please enter a whole number between 1 and 100");
+                    userNum = int.Parse(Console.ReadLine());
+                }
+
+                if (userNum > 0 && userNum <= 100)
+                {
+                    validNum = true;
+
+                    while (validNum)
+                    {
+                        if (userNum % numAnalyzer > 0)
+                        {
+                            Console.WriteLine(userNum + " odd.");
+                            validNum = false;
+                        }
+                        else if (userNum % numAnalyzer == 0 && userNum >= 2 && userNum < 25)
+                        {
+                            Console.WriteLine("even and less than 25");
+                            validNum = false;
+                        }
+                        else if (userNum % numAnalyzer == 0 && userNum > 25)
+                        {
+                            Console.WriteLine(userNum + " even.");
+                            validNum = false;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Would you like to check another number? (y/n)");
+                string checkAgain = Console.ReadLine();
+                checkAgain.ToLower();
+
+                if (checkAgain == "y")
+                    again = true;
+                else if (checkAgain == "n")
+                    again = false;
+                else
+                {
+                    Console.WriteLine("Would you like to check another number? (y/n)");
+                    checkAgain = Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
